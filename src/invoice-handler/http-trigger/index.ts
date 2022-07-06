bb73c69b97e0649fe36633e8ac43637b8fc6c66b
André Reis <arrr.cin@gmail.com>
2022-07-06 01:15:33 -0400

    Add web scraper using puppeteer.

--- src/invoice-handler/http-trigger/index.ts
+++ src/invoice-handler/http-trigger/index.ts
@@ -1,17 +1,19 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { Scraper } from "../lib";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    console.log('************ STARTING **************')
+
+    const scraper = new Scraper('https://www.futbin.com');
+    const playerName = await scraper.load('22/player/26054', '.header_name');
+
+    console.log('name: ' + playerName);
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: playerName,
     };
-
 };
 
 export default httpTrigger;
---
+++ src/invoice-handler/lib/scraper.ts
@@ -0,0 +1,52 @@
+import * as puppeteer from 'puppeteer';
+
+export class Scraper {
+    private baseUrl: string;
+
+    constructor(baseUrl: string) {
+        this.baseUrl = baseUrl;
+    }
+
+    load = async (path: string, query: string) => {
+        const browser = await puppeteer.launch({
+            headless: true,
+            devtools: false,
+            defaultViewport: {
+                width: 1024 + Math.floor(Math.random() * 100),
+                height: 768 + Math.floor(Math.random() * 100),
+            }
+        });
+        const page = await browser.newPage();
+        const fakeUserAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko';
+
+        await page.evaluateOnNewDocument(fakeUserAgent => {
+            let open = window.open;
+            window.open = (...args) => {
+                let newPage = open(...args);
+                Object.defineProperty(newPage.navigator, 'userAgent', { get: () => fakeUserAgent });
+                return newPage;
+            }
+
+            window.open.toString = () => 'function open() { [native code] }';
+        }, fakeUserAgent);
+
+        await page.setUserAgent(fakeUserAgent);
+
+        const url = `${this.baseUrl}/${path}`;
+
+        const cookies = await page.cookies(url);
+        await page.deleteCookie(...cookies);
+
+        await page.goto(url, { waitUntil: 'load' });
+        const pageData = await page.evaluate((sel: string) => {
+            const divCount = document.querySelectorAll(sel).length;
+            return divCount;
+        }, query);
+
+        await page.close();
+        await browser.close();
+
+        return pageData;
+    }
+
+}
