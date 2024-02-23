310bbeaffafbce158748d53b97971cc7fbd4cc19
André Reis <arrr.cin@gmail.com>
2024-02-22 22:17:28 -0300

    Add a underscore to unused param

--- src/middleware/pager.middleware.ts
+++ src/middleware/pager.middleware.ts
@@ -2,7 +2,7 @@ import { Logger, NestMiddleware } from '@nestjs/common';
 
 export class PagerMiddleware implements NestMiddleware {
   private readonly logger = new Logger(PagerMiddleware.name);
-  use(req: any, res: any, next: (error?: any) => void) {
+  use(req: any, _res: any, next: (error?: any) => void) {
     req.query.limit = Math.abs(+req.query.limit) || 10;
     req.query.page = Math.abs(+req.query.page) || 1;
 
