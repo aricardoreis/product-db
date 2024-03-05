fb1ba001af31a456ee451951964d3e02799bc6d7
André Reis <arrr.cin@gmail.com>
2024-03-05 19:51:05 -0300

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { TransformInterceptor } from './shared/transform-interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
 }
