71f6ef6b614e72b54ab05980acbd47437e37000b
André Reis <arrr.cin@gmail.com>
2024-03-12 15:32:22 -0400

    Update product (#16)
    One product can be renamed

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Query } from '@nestjs/common';
 import { Product } from './entities/product.entity';
 import { ProductsService } from './products.service';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -20,5 +21,13 @@ export class ProductsController {
   @Get('/:id')
   async findOne(@Param('id') id: number): Promise<Product> {
     return this.productsService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsService.update(+id, updateProductDto);
   }
 }
