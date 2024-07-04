fdb0783296bb6ccfa89113ba14e0542ea20882d0
André Reis <arrr.cin@gmail.com>
2024-07-04 19:07:15 -0400

    Add tests to the auth controller and service

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -5,13 +5,16 @@ import { serviceMock } from './../../test/mocks';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  const authServiceMock = {
+    signIn: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
           provide: AuthService,
-          useValue: serviceMock,
+          useValue: authServiceMock,
         },
       ],
       controllers: [AuthController],
@@ -22,5 +25,27 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('test method should return true', async () => {
+    expect(controller.test).toBeDefined();
+
+    expect(await controller.test()).toEqual(true);
+  });
+
+  it('should login and return token', async () => {
+    const responseData = {
+      access_token: '123456798',
+      token_type: 'bearer',
+      expires_in: 3600,
+      expires_at: 1720217827,
+      refresh_token: '12345',
+    };
+
+    authServiceMock.signIn.mockReturnValue(responseData);
+
+    expect(
+      await controller.signIn({ email: 'email@email.com', password: '132456' }),
+    ).toEqual(responseData);
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,15 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { SupabaseService } from '../shared/supabase/supabase.service';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+
+  const supabaseServiceMock = { getClient: jest.fn() };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         AuthService,
-        { provide: SupabaseService, useValue: { fetchData: jest.fn() } },
+        { provide: SupabaseService, useValue: supabaseServiceMock },
       ],
     }).compile();
 
@@ -18,5 +21,37 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return access token when credentials match', async () => {
+    const token = '132456789';
+    const authTokenResponseData = {
+      data: { session: { access_token: token } },
+    };
+
+    supabaseServiceMock.getClient.mockReturnValue({
+      auth: {
+        signInWithPassword: jest.fn().mockResolvedValue(authTokenResponseData),
+      },
+    });
+
+    const { access_token } = await service.signIn('test', 'test');
+    expect(access_token).toEqual(token);
+  });
+
+  it('should throw UnauthorizedException when credentials do not match', async () => {
+    const authTokenResponseData = {
+      error: { message: 'Invalid credentials' },
+    };
+
+    supabaseServiceMock.getClient.mockReturnValue({
+      auth: {
+        signInWithPassword: jest.fn().mockResolvedValue(authTokenResponseData),
+      },
+    });
+
+    await expect(service.signIn('test', 'test')).rejects.toThrowError(
+      UnauthorizedException,
+    );
   });
 });
